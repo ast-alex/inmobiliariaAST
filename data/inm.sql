d73595e70704b1e81f0e37d398c0fbe5b961c8f4
alex-astudillo <105954347+ast-alex@users.noreply.github.com>
2024-09-12 16:17:35 -0300

    dbm

--- data/inm.sql
+++ data/inm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-08-2024 a las 03:57:51
+-- Tiempo de generación: 12-09-2024 a las 21:15:53
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -18,8 +18,34 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `inmobiliaria_e1`
+-- Base de datos: `inm`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inmueble`
+--
+
+CREATE TABLE `inmueble` (
+  `ID_inmueble` int(11) NOT NULL,
+  `ID_propietario` int(11) DEFAULT NULL,
+  `Direccion` varchar(255) NOT NULL,
+  `Uso` enum('Comercial','Residencial') NOT NULL,
+  `Tipo` enum('Local','Deposito','Casa','Departamento','Otro') NOT NULL,
+  `Cantidad_Ambientes` int(11) NOT NULL,
+  `Latitud` decimal(9,6) DEFAULT NULL,
+  `Longitud` decimal(9,6) DEFAULT NULL,
+  `Precio` decimal(10,2) NOT NULL,
+  `Estado` enum('Disponible','No disponible') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `inmueble`
+--
+
+INSERT INTO `inmueble` (`ID_inmueble`, `ID_propietario`, `Direccion`, `Uso`, `Tipo`, `Cantidad_Ambientes`, `Latitud`, `Longitud`, `Precio`, `Estado`) VALUES
+(1, 2, 'Calle del Ejemplo 456', 'Comercial', 'Departamento', 4, 40.123456, -3.654323, 1200.00, 'Disponible');
 
 -- --------------------------------------------------------
 
@@ -42,8 +68,7 @@ CREATE TABLE `inquilino` (
 --
 
 INSERT INTO `inquilino` (`ID_inquilino`, `DNI`, `Nombre`, `Apellido`, `Telefono`, `Email`, `Direccion`) VALUES
-(1, '3300021', 'Ricky', 'Centurion', '2664033442', 'notengosueno@gmail.com', 'La suerte es loca'),
-(2, '32323232', 'Editame', 'LA PILDORA', '32', 'apkrey32@gmail.com', 'Perfumito');
+(1, '87654321B', 'Ana', 'García', '555-5678', 'ana.garcia@example.com', 'Avenida Siempre Viva 742');
 
 -- --------------------------------------------------------
 
@@ -66,12 +91,19 @@ CREATE TABLE `propietario` (
 --
 
 INSERT INTO `propietario` (`ID_propietario`, `DNI`, `Nombre`, `Apellido`, `Telefono`, `Email`, `Direccion`) VALUES
-(1, '322', 'Probando ', 'ElPRI', '123', 'a@gmail..com', 'Av El primeo'),
-(3, '322422', 'Editame', 'AGORA SIM', '123', 'a@gmail..com.ar', 'EL PRIME');
+(1, '12345678A', 'Juan', 'Pérez', '555-1234', 'juan.perez@example.com', 'Calle Falsa 123'),
+(2, '32323232', 'Pepito', 'Perez', '2665987002', 'a@gmail.com', 'Av LaFinur 132');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `inmueble`
+--
+ALTER TABLE `inmueble`
+  ADD PRIMARY KEY (`ID_inmueble`),
+  ADD KEY `ID_propietario` (`ID_propietario`);
 
 --
 -- Indices de la tabla `inquilino`
@@ -90,16 +122,32 @@ ALTER TABLE `propietario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `inmueble`
+--
+ALTER TABLE `inmueble`
+  MODIFY `ID_inmueble` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `inquilino`
 --
 ALTER TABLE `inquilino`
-  MODIFY `ID_inquilino` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID_inquilino` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `propietario`
 --
 ALTER TABLE `propietario`
-  MODIFY `ID_propietario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID_propietario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `inmueble`
+--
+ALTER TABLE `inmueble`
+  ADD CONSTRAINT `inmueble_ibfk_1` FOREIGN KEY (`ID_propietario`) REFERENCES `propietario` (`ID_propietario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
